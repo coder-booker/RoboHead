ac56f1f580ea0935fd2fb2776796153751e40cd1
coder-booker <54313009+coder-booker@users.noreply.github.com>
2024-07-29 23:40:56 +0800

    update

--- face/src/constants/ip.js
+++ face/src/constants/ip.js
@@ -1,4 +1,4 @@
-// const LOCALHOST = `http://localhost`;
-const LOCALHOST = `http://192.168.56.1`; // phone on win
+const LOCALHOST = `http://localhost`;
+// const LOCALHOST = `http://192.168.56.1`; // phone on win
 // const LOCALHOST = `http://192.168.234.128`; // phone on pi
 export default LOCALHOST;
