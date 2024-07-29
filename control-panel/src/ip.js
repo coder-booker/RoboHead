9625df72ec11f4f110fd15aef104c903228a333d
coder-booker <54313009+coder-booker@users.noreply.github.com>
2024-07-30 03:08:14 +0800

    change to pi ip mode

--- control-panel/src/ip.js
+++ control-panel/src/ip.js
@@ -1,4 +1,4 @@
-const LOCALHOST = `http://localhost`;
-// const LOCALHOST = `http://192.168.144.69`; // phone on pi
+// const LOCALHOST = `http://localhost`;
+const LOCALHOST = `http://192.168.144.69`; // phone on pi
 // const LOCALHOST = `http://192.168.234.128`; // phone on pi
 export default LOCALHOST;
